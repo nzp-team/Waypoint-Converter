7fd8279d11d1d8f459bc77287a2c6731f874bcc8
BCDeshiG <bcdg55@gmail.com>
2022-09-07 20:16:00 +0100

    Detect format

--- waycon.py
+++ waycon.py
@@ -16,9 +16,26 @@ def startConversion(inputFile, outputFile):
 	if not(outputFile.lower().endswith(".way")):
 		outputFile += ".way"
 	# Read in old waypoints
-	tempF = open(inputFile, "r")
-	oldWayFile = tempF.read()
-	tempF.close()
-	print(oldWayFile)
+	with open(inputFile, "r") as tempF:
+		oldWayFile = tempF.readlines()
+
+	# Check format
+	header = oldWayFile[0].strip("\n")
+	if header == "Waypoint": # PSP
+		print("PSP format detected")
+	elif header == "waypoint": # PC
+		print("PC format detected")
+	else: # Check if BETA or just random crap
+		where = header.strip("'").split() # Split xyz
+		# Should look something like [-40.5, 12.2, 8.0]
+		if len(where) != 3:
+			print("Invalid waypoint file") # Clearly not co-ords
+		else:
+			try: # Check if they're actual numbers
+				for i in range(len(where)):
+					float(where[i])
+				print("BETA format detected")
+			except ValueError:
+				print("Invalid waypoint co-ords")
 
 parseArgs()
